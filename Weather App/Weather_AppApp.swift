c58684d059df3441e0cbd83bf4f651219ca0a19e
neshwa.parveen <neshwa.parveen@chillarcards.com>
2023-11-29 12:44:20 +0530

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Neshwa on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
